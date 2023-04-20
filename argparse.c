485dd0afc94d36bd3611daa4d79bc98c792e869b
garyhsu <garyhsu1209@gmail.com>
2023-04-20 06:06:31 +0000

    list root dir

--- argparse.c
+++ argparse.c
@@ -8,6 +8,36 @@
 
 #include "argparse.h"
 
+#define DEBUG 0
+
+void convert_filename(unsigned char *src, char *dest) {
+    char name[9];
+    char ext[4];
+    int i = 0;
+    for (i = 0; i < 8; i++) {
+        if (src[i] == ' ') break;
+        name[i] = src[i];
+    }
+    name[i] = '\0';
+
+    for (i = 8; i < 11; i++) {
+        if (src[i] == ' ') break;
+        ext[i - 8] = src[i];
+    }
+    ext[i - 8] = '\0';
+    if (strlen(ext) > 0) {
+        sprintf(dest, "%s.%s", name, ext);
+    } else {
+        sprintf(dest, "%s", name);
+    }
+
+}
+
+int convert_cluster(unsigned short high, unsigned short low) {
+    unsigned int clus_num = high;
+    clus_num = (clus_num << 16) | low;
+    return (int)clus_num;
+}
 
 void open_disk(char *disk_name, void **data) {
     int fd = open(disk_name, O_RDWR);
@@ -23,6 +53,14 @@ void open_disk(char *disk_name, void **data) {
 }
 
 
+int get_fat_entry(void *data, unsigned int clus_num) {
+    BootEntry *ptr = (BootEntry *)data;
+    unsigned int reserved_area = ptr->BPB_BytsPerSec * ptr->BPB_RsvdSecCnt;
+    unsigned char *entry = (unsigned char*)data + reserved_area;
+    int *fat = (int*) entry;
+    return fat[clus_num];
+}
+
 void print_fs_info(void *data) {
     BootEntry *ptr = (BootEntry *)data;
     printf("Number of FATs = %d\n", ptr->BPB_NumFATs);
@@ -32,13 +70,51 @@ void print_fs_info(void *data) {
 }
 
 
-// void list_root_dir(void *data) {
-//     BootEntry *ptr = (BootEntry *)data;
-//     unsigned int clus = ptr->BPB_RootClus;
-//     while (clus < 0x0ffffff8) {
+void list_root_dir(void *data) {
+    BootEntry *ptr = (BootEntry *)data;
+    unsigned int root_clus_num = ptr->BPB_RootClus;
+    unsigned int clus_num = root_clus_num;
+    int num_entries = 0;
+    while (clus_num < 0x0ffffff8) {
+        unsigned int reserved_area_size = ptr->BPB_BytsPerSec * ptr->BPB_RsvdSecCnt;
+        unsigned int fat_area_size = ptr->BPB_NumFATs * ptr->BPB_FATSz32 * ptr->BPB_BytsPerSec; 
+        unsigned char *clus_data = (unsigned char *)data + reserved_area_size + fat_area_size + ((clus_num - 2) * ptr->BPB_SecPerClus * ptr->BPB_BytsPerSec);
+        if (DEBUG) {
+            printf("reserved area size :%x\n", reserved_area_size);
+            printf("fat area size :%x\n", fat_area_size);
+            printf("clus_num :%x\n",clus_num);
+            printf("clus_data: %x\n", reserved_area_size + fat_area_size + ((clus_num - 2) * ptr->BPB_SecPerClus * ptr->BPB_BytsPerSec));
+        }
 
-//     }
-// }
+        for (int i = 0; i < ptr->BPB_SecPerClus * ptr->BPB_BytsPerSec; i+=32) {
+            DirEntry *dir = (DirEntry *)(clus_data + i);
+            if (dir->DIR_Name[0] == 0xE5 || dir->DIR_Name[0] == 0x00) {
+                continue;
+            }
+            char filename[13];
+            convert_filename(dir->DIR_Name, filename);
+            int start_clus = convert_cluster(dir->DIR_FstClusHI, dir->DIR_FstClusLO);
+            if (dir->DIR_Attr == 0x10) {
+                // Is a directory
+                printf("%s/ (starting cluster = %d)\n", filename, start_clus);
+            } else if (dir->DIR_Attr == 0x0f){
+                // Long file name
+                perror("Should not have a long file here\n");
+                ;
+            } else {
+                // Is a file
+                if (dir->DIR_FileSize == 0) {
+                    printf("%s (size = 0)\n", filename);
+                } else {
+                    printf("%s (size = %d, starting cluster = %d)\n", filename, dir->DIR_FileSize, start_clus);
+                }
+            }
+            num_entries++;
+        }
+        clus_num = get_fat_entry(data, clus_num);
+    }
+    printf("Total number of entries = %d\n", num_entries);
+}
 
 
 void print_usage() {
@@ -117,7 +193,6 @@ void parse_args(int argc, char *argv[], char *disk_name) {
         print_usage();
         exit(1);
     }
-    
     strcpy(disk_name, argv[optind]);
     
     validate_flags(flag_i, flag_l, flag_r, flag_R, flag_s);
@@ -129,6 +204,6 @@ void parse_args(int argc, char *argv[], char *disk_name) {
     
     if (flag_l) {
         open_disk(disk_name, &data);
-        // list_root_dir(data);
+        list_root_dir(data);
     }
 }
